b309014abe5fea5c3b7d355d8b2003fe5a154fb8
gustavo <gustavo38000@gmail.com>
2024-11-01 20:49:20 -0300

    First date?

---
+++ script.js
@@ -0,0 +1,15 @@
+const no_btn = document.querySelector("#no-btn");
+const yes_btn = document.querySelector("#yes-btn");
+
+no_btn.addEventListener('click', ()=>{
+    no_btn.style.position = "absolute";
+    no_btn.style.marginLeft = Math.random() * 500 + "px";
+    no_btn.style.marginTop = Math.random() + Math.random() * 500 + "px"; 
+})
+
+yes_btn.addEventListener("click", ()=>{
+    const aceitou = confirm("Certeza? :| (Ok = sim, Cancelar = não)");
+    if(aceitou == true){
+        alert("Quase lá. Só não quero te enganar, mas ao mesmo tempo não é justo de fazer esperar.");
+    }
+})
